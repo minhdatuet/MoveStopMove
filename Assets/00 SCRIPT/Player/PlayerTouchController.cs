3955269c7b0fe94ad97fac2c780f13c1cef125cc
minhdatuet <minhdatuet@gmail.com>
2024-07-01 16:57:18 +0700

    Upload project

---
+++ Assets/00 SCRIPT/Player/PlayerTouchController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerTouchController : MonoBehaviour
+{
+    public float speed = 5f;
+    private Vector2 moveDirection;
+    private CharacterController controller;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // Chuyển đổi hướng 2D thành 3D
+        Vector3 move = new Vector3(moveDirection.x, 0, moveDirection.y);
+
+        // Di chuyển nhân vật
+        controller.Move(move * speed * Time.deltaTime);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Debug.Log(context.phase);
+        if (context.phase == InputActionPhase.Performed)
+        {
+            // Nhận hướng di chuyển từ input
+            moveDirection = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            // Khi người chơi bỏ tay khỏi màn hình, dừng di chuyển
+            moveDirection = Vector2.zero;
+        }
+    }
+}
