36667120c9836ff8f522108921fe2352135a6d3a
Le Minh Dat <minhdatuet@gmail.com>
2024-07-19 15:54:35 +0700

    complete weapon shop

--- Assets/00 SCRIPT/UI/WeaponControl/SelectWeaponController.cs
+++ Assets/00 SCRIPT/UI/WeaponControl/SelectWeaponController.cs
@@ -1,6 +1,7 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -12,19 +13,48 @@ public class SelectWeaponController : MonoBehaviour
     [SerializeField] Text selectText;
     [SerializeField] Text costText;
     [SerializeField] Button selectButton;
+    [SerializeField] Button unlockButton;
     [SerializeField] Button buyButton;
     [SerializeField] List<GameObject> weapons = new List<GameObject>();
     [SerializeField] GameObject weaponInHand;
     [SerializeField] GameObject weaponColorContainer;
+    [SerializeField] List<Button> weaponColorList = new List<Button>();
+    [SerializeField] Text lockText;
     GameData gameData;
-
+    GameObject tryingWeapon;
+    GameObject tryingContainer;
+    GameObject tryingWeaponColor;
     // Start is called before the first frame update
     void Start()
     {
-        gameData = SaveLoadManager.Instance.LoadData();
-        SetSelectedText(0);
-        CheckButtonDisplay(0);
+        SetBeginWeapon();
+    }
 
+    private void OnEnable()
+    {
+        SetBeginWeapon();
+    }
+
+    void SetBeginWeapon()
+    {
+        gameData = SaveLoadManager.Instance.LoadData();
+
+        //Thiết lập các button
+        for (int i = 0; i < weapons.Count; i++)
+        {
+            if (weapons[i].gameObject.activeInHierarchy)
+            {
+                tryingWeapon = weapons[i].gameObject;
+            }
+        }
+        for (int i = 0; i < weaponColorList.Count; i++)
+        {
+            Button currentButton = weaponColorList[i];
+            currentButton.onClick.AddListener(() => TryWeaponColor(currentButton));
+            currentButton.gameObject.GetComponent<Outline>().enabled = false;
+        }
+
+        //Thiết lập weapon hiển thị khi mở 
         if (weapons.Count != 0)
         {
             int len = weapons.Count;
@@ -33,6 +63,9 @@ public class SelectWeaponController : MonoBehaviour
                 if (gameData.player.weapon[i].enable)
                 {
                     weapons[i].gameObject.SetActive(true);
+                    CheckButtonDisplay(i);
+                    CheckLocked(i);
+                    CheckCanBuy(i);
                     weaponNameText.text = weapons[i].name.ToUpper();
                     for (int j = 0; j < weaponColorContainer.transform.childCount; j++)
                     {
@@ -43,7 +76,8 @@ public class SelectWeaponController : MonoBehaviour
                             if (k == i)
                             {
                                 currWeaponColor.SetActive(true);
-                            } else
+                            }
+                            else
                             {
                                 currWeaponColor.SetActive(false);
                             }
@@ -52,6 +86,7 @@ public class SelectWeaponController : MonoBehaviour
                         {
                             currColor.GetComponent<Outline>().enabled = true;
                             weapons[i].GetComponent<Renderer>().materials = currColor.transform.GetChild(0).transform.GetChild(i).transform.gameObject.GetComponent<Renderer>().materials;
+                            SetSelectedText(j);
                         }
                         else
                         {
@@ -59,7 +94,8 @@ public class SelectWeaponController : MonoBehaviour
                         }
                     }
                     SetWeaponInfo(i);
-                } else
+                }
+                else
                 {
                     weapons[i].gameObject.SetActive(false);
                 }
@@ -67,16 +103,67 @@ public class SelectWeaponController : MonoBehaviour
         }
     }
 
-    void SetSelectedText(int weaponIndex)
+    void TryWeaponColor(Button clickedButton)
     {
-        //if (gameData.player.weapon[weaponIndex].enable && gameData.player.weapon)
-        //{
-        //    selectText.text = "Equipped";
-        //} else
-        //{
-        //    selectText.text = "Select";
-        //}
-        
+        for (int i = 0; i < weapons.Count; i++)
+        {
+            if (weapons[i].gameObject.activeInHierarchy)
+            {
+                tryingWeapon = weapons[i].gameObject;
+            }
+        }
+
+        for (int i = 0; i < weaponColorList.Count; i++)
+        {
+            Button currentButton = weaponColorList[i];
+            if (clickedButton == currentButton)
+            {
+                SetSelectedText(i);
+                clickedButton.gameObject.GetComponent<Outline>().enabled = true;
+            }
+            else
+            {
+                currentButton.gameObject.GetComponent<Outline>().enabled = false;
+            }
+        }
+        tryingContainer = clickedButton.transform.GetChild(0).gameObject;
+        for (int i = 0; i < tryingContainer.transform.childCount; i++)
+        {
+            if (tryingContainer.transform.GetChild(i).gameObject.activeInHierarchy)
+            {
+                tryingWeaponColor = tryingContainer.transform.GetChild(i).gameObject;
+                tryingWeapon.GetComponent<Renderer>().materials = tryingWeaponColor.GetComponent<Renderer>().materials;
+                break;
+            }
+        }
+    }
+
+
+    void SetSelectedText(int colorIndex)
+    {
+        for (int i = 0; i < weapons.Count; i++)
+        {
+            if (weapons[i].gameObject.activeInHierarchy)
+            {
+                if (gameData.player.weapon[i].color[colorIndex].isUnLocked)
+                {
+                    selectButton.gameObject.SetActive(true);
+                    unlockButton.gameObject.SetActive(false);
+                    if (gameData.player.weapon[i].color[colorIndex].enable && gameData.player.weapon[i].enable)
+                    {
+                        selectText.text = "Equipped";
+                    } else
+                    {
+                        selectText.text = "Select";
+                    }
+                } else
+                {
+                    selectButton.gameObject.SetActive(false);
+                    unlockButton.gameObject.SetActive(true);
+                }
+            }
+        }
+
     }
 
     public void SetWeaponInfo(int weaponIndex)
@@ -90,11 +177,32 @@ public class SelectWeaponController : MonoBehaviour
         if (gameData.player.weapon[weaponIndex].hasBought)
         {
             buyButton.gameObject.SetActive(false);
-            selectButton.gameObject.SetActive(true);
+            selectButton.transform.parent.gameObject.SetActive(true);
         } else
         {
             buyButton.gameObject.SetActive(true);
-            selectButton.gameObject.SetActive(false);
+            selectButton.transform.parent.gameObject.SetActive(false);
+        }
+    }
+
+    void CheckLocked(int weaponIndex)
+    {
+        for (int i = 0; i < weaponColorList.Count; i++)
+        {
+            if (i == 2 || i == 3)
+            {
+                if (gameData.player.weapon[weaponIndex].color[i].isUnLocked)
+                {
+                    weaponColorList[i].transform.GetChild(1).gameObject.SetActive(false);
+                } else
+                {
+                    weaponColorList[i].transform.GetChild(1).gameObject.SetActive(true);
+                }
+            }
+            else
+            {
+                continue;
+            }
         }
     }
 
@@ -109,10 +217,11 @@ public class SelectWeaponController : MonoBehaviour
                 {
                     weaponNameText.text = weapons[i-1].name.ToUpper();
                     SetWeaponInfo(i-1);
-                    SetSelectedText(i - 1);
                     CheckButtonDisplay(i - 1);
+                    CheckLocked(i - 1);
                     weapons[i].SetActive(false);
                     weapons[i-1].SetActive(true);
+                    CheckCanBuy(i - 1);
                     for (int j = 0; j < weaponColorContainer.transform.childCount; j++)
                     {
                         GameObject currColor = weaponColorContainer.transform.GetChild(j).gameObject;
@@ -133,6 +242,7 @@ public class SelectWeaponController : MonoBehaviour
                         if (gameData.player.weapon[i-1].color[j].enable)
                         {
                             currColor.GetComponent<Outline>().enabled = true;
+                            SetSelectedText(j);
                             weapons[i-1].GetComponent<Renderer>().materials = currColor.transform.GetChild(0).transform.GetChild(i - 1).transform.gameObject.GetComponent<Renderer>().materials;
                         }
                         else
@@ -140,6 +250,7 @@ public class SelectWeaponController : MonoBehaviour
                             currColor.GetComponent<Outline>().enabled = false;
                         }
                     }
+
                 }
                 break;
 
@@ -158,8 +269,8 @@ public class SelectWeaponController : MonoBehaviour
                 {
                     weaponNameText.text = weapons[i + 1].name.ToUpper();
                     SetWeaponInfo(i + 1);
-                    SetSelectedText(i + 1);
                     CheckButtonDisplay(i + 1);
+                    CheckLocked(i + 1);
                     weapons[i].SetActive(false);
                     weapons[i + 1].SetActive(true);
                     for (int j = 0; j < weaponColorContainer.transform.childCount; j++)
@@ -182,6 +293,7 @@ public class SelectWeaponController : MonoBehaviour
                         if (gameData.player.weapon[i+1].color[j].enable)
                         {
                             currColor.GetComponent<Outline>().enabled = true;
+                            SetSelectedText(j);
                             weapons[i+1].GetComponent<Renderer>().materials = currColor.transform.GetChild(0).transform.GetChild(i+1).transform.gameObject.GetComponent<Renderer>().materials;
                         }
                         else
@@ -189,6 +301,7 @@ public class SelectWeaponController : MonoBehaviour
                             currColor.GetComponent<Outline>().enabled = false;
                         }
                     }
+                    CheckCanBuy(i + 1);
                 }
                 break;
 
@@ -207,7 +320,6 @@ public class SelectWeaponController : MonoBehaviour
                 weaponInHand.transform.GetChild(i).gameObject.SetActive(true);
                 SetWeaponColor(i);
                 gameData.player.weapon[i].enable = true;
-                SetSelectedText(i);
 
             } else
             {
@@ -241,18 +353,66 @@ public class SelectWeaponController : MonoBehaviour
             if (weapons[i].gameObject.activeInHierarchy)
             {
                 int skinCost = Int32.Parse(buyButton.gameObject.GetComponentInChildren<Text>().text);
-                if (gameData.player.weapon[i].cost >= skinCost)
+                if (gameData.coin >= skinCost)
                 {
-                    gameData.player.weapon[i].cost -= skinCost;
-
+                    gameData.coin -= skinCost;
                     gameData.player.weapon[i].hasBought = true;
                     SaveLoadManager.Instance.SaveData(gameData);
                     CheckButtonDisplay(i);
+                    CheckCanBuy(i);
                     break;
                 }
             }
         }
         StartCoroutine(MenuUIManager.Instance.SetDataCoroutine());
 
+    }
+
+    public void UnlockColor()
+    {
+        for (int i = 0; i < weapons.Count; i++)
+        {
+            if (weapons[i].gameObject.activeInHierarchy)
+            {
+                for (int j = 0; j < weaponColorList.Count; j++)
+                {
+                    if (weaponColorList[j].gameObject.GetComponent<Outline>().enabled)
+                    {
+                        gameData.player.weapon[i].color[j].isUnLocked = true;
+                        SaveLoadManager.Instance.SaveData(gameData);
+                        CheckLocked(i);
+                        SetSelectedText(j);
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    void CheckCanBuy(int weaponIndex)
+    {
+        if (gameData.player.weapon[weaponIndex].hasBought)
+        {
+            weaponColorContainer.gameObject.SetActive(true);
+            lockText.gameObject.SetActive(false);
+
+        } else
+        {
+            weaponColorContainer.gameObject.SetActive(false);
+            lockText.gameObject.SetActive(true);
+            if (weaponIndex > 0)
+            {
+                if (!gameData.player.weapon[weaponIndex - 1].hasBought)
+                {
+                    lockText.text = "(Unlock " + weapons[weaponIndex - 1].name + " first)";
+                    buyButton.gameObject.SetActive(false);
+                }
+                else
+                {
+                    lockText.text = "(Lock)";
+                    buyButton.gameObject.SetActive(true);
+                }
+            }
+        }
     }
 }
